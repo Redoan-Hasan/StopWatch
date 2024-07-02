619b0307d3f77abc9099014bb833ebaeecb37c18
Redoan Hasan <redoanhasan57@gmail.com>
2024-07-02 07:07:57 +0600

    fully complete

--- scripts.js
+++ scripts.js
@@ -7,7 +7,10 @@ let handleInterval = null;
 const stopWatchContainer = document.getElementById('timer-display');
 
 function startWatch(){
-    handleInterval = setInterval(displayTime,1);
+    if(handleInterval !== null){
+        clearInterval(handleInterval)
+    }
+    handleInterval = setInterval(displayTime,10);
 };
 function pauseWatch(){
     clearInterval(handleInterval);
@@ -19,11 +22,11 @@ function resetWatch(){
     minutes =0;
     hours = 0;
     // [millisecond, sec , minutes,hours] = [0,0,0,0]
-    stopWatchContainer.innerHTML = "00 : 00 : 00 : 000";
+    stopWatchContainer.innerHTML = " 00 : 00 : 00 : 000 ";
 };
 // main 
 function displayTime(){
-    millisecond+=1;
+    millisecond+=10;
     if(millisecond === 1000){
         millisecond = 0;
         sec++;
@@ -36,15 +39,15 @@ function displayTime(){
             }
         }
     }
-    hours = hours < 10? "0" + hours: hours;
-    minutes = minutes < 10? "0"+ minutes: minutes;
-    sec = sec < 10? "0" + sec : sec;
-    millisecond = 
+    let ho = hours < 10? "0" + hours: hours;
+    let mi = minutes < 10? "0"+ minutes: minutes;
+    let se = sec < 10? "0" + sec : sec;
+    let mil = 
                     millisecond<10? 
                     "00"+ millisecond:
                     millisecond<100? 
                     "0" + millisecond:
                     millisecond;
     // stopWatchContainer.innerHTML = `${hours} : ${minutes} : ${sec} : ${millisecond} `;
-    stopWatchContainer.innerHTML = hours + ":" + minutes + ":" + sec + ":" + millisecond ;
+    stopWatchContainer.innerHTML = ho + ":" + mi + ":" + se + ":" + mil ;
 }
